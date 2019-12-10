d61e0cfcdf14d235893fd7cddbd0a0afa1d9cc73
sarah <s.j.condon01@gmail.com>
2019-12-10 16:27:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_accessor :id, :name, :grade
 
 
-  def initialize (id = nil, name, grade,) 
+  def initialize (id = nil, name, grade) 
     @name = name
     @grade = grade
     @id = id
